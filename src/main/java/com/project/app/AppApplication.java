a27f0eef9e946887fb4e30f2c1305e0e5bae4dd2
youngnam81-kim <kimyn81@naver.com>
2025-10-15 07:12:36 +0900

    Initial commit

---
+++ src/main/java/com/project/app/AppApplication.java
@@ -0,0 +1,15 @@
+package com.project.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {BatchAutoConfiguration.class})
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
